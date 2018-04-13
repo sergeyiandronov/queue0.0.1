e99272252fd92968f7ff57979bbda1ed336d7229
sergeyiandronov <sergeyiandronov@gmail.com>
2018-04-13 20:49:05 +0300

    travis

--- tests/queue.cpp
+++ tests/queue.cpp
@@ -20,3 +20,18 @@ TEST_CASE("pop&push")
     
     REQUIRE( "1" == ostream.str() );
 }
+TEST_CASE("copy")
+{
+    queue_t<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    
+   
+    std::ostringstream ostream;
+    queue_t<int> q1=q;
+    
+    ostream<<q1.pop();
+    
+    REQUIRE( "1" == ostream.str() );
+}
